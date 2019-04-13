20d2d5e21dfdb7699087612f9318efab926cdb18
IharTsykala <sudja09@bk.ru>
2019-04-14 02:52:06 +0300

    feat: add solution 3/5 date tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+  return new Date(value);
 }
 
 
@@ -56,7 +56,8 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+  let year = date.getFullYear();
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);  
 }
 
 
@@ -76,7 +77,27 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+  //  console.log(startDate, endDate)
+
+   
+   
+  //  let res = endDate.getTime() - startDate.getTime()
+ 
+  //  let date = new Date(res);
+  
+  //   var dd = date.getHours();
+  //   if (dd < 10) dd = '0' + dd;
+  
+  //   var mm = date.getMinutes();
+  //   if (mm < 10) mm = '0' + mm;
+  
+  //   var yy = date.getSeconds();
+  //   if (yy < 10) yy = '0' + yy;
+  
+  //   return  dd + '.' + mm + '.' + yy;
+    
+  //  console.log(date);
+  throw new Error('Not implemented');
 }
 
 
